0c84cf9d12c2dd8e4cff3b791b595eff73c9e2c3
David Gomes <davidbgomes92@gmail.com>
2019-10-24 22:37:53 +0100

    Deleted App and replaced with Home

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function app (props){
-   return{
-      <div>
-         <p>Random shit</p>
-      </div>
-   }
-}
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App (props){
+   return{
+      <div>
+         <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href=""
+         </nav>
+      </div>
+   }
+}
