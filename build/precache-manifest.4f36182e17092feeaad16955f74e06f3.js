d3a1d97723f24f4084ca0e74da5301b21a4498fd
David Gomes <davidbgomes92@gmail.com>
2020-11-16 14:59:22 +0000

    commiting final build

---
+++ build/precache-manifest.4f36182e17092feeaad16955f74e06f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c805b394188f2240e9c1e127c6f46cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93607ac8082dd529affe",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "faedb62c2a9582986e21",
+    "url": "/static/css/main.4f695af4.chunk.css"
+  },
+  {
+    "revision": "93607ac8082dd529affe",
+    "url": "/static/js/2.bb7dc959.chunk.js"
+  },
+  {
+    "revision": "faedb62c2a9582986e21",
+    "url": "/static/js/main.e06035c8.chunk.js"
+  },
+  {
+    "revision": "05129d9571d833ef1980",
+    "url": "/static/js/runtime-main.a5eaf332.js"
+  }
+]);
