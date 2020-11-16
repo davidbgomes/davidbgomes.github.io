c350c79f7cadfc95179d52327ba261552c37bed5
David Gomes <davidbgomes92@gmail.com>
2020-11-16 22:55:49 +0000

    Updates

---
+++ precache-manifest.6d1217abc766407d66fc38b4ea9f1688.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa76f71bded5e7ed7fe40b5a1c6b3124",
+    "url": "/index.html"
+  },
+  {
+    "revision": "481b234168a81e58e11a",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "39e3acf8b2e4e34d5ceb",
+    "url": "/static/css/main.9db939a7.chunk.css"
+  },
+  {
+    "revision": "481b234168a81e58e11a",
+    "url": "/static/js/2.15911a69.chunk.js"
+  },
+  {
+    "revision": "39e3acf8b2e4e34d5ceb",
+    "url": "/static/js/main.7f71dc13.chunk.js"
+  },
+  {
+    "revision": "05129d9571d833ef1980",
+    "url": "/static/js/runtime-main.a5eaf332.js"
+  }
+]);
