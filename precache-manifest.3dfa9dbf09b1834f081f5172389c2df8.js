cc71fcc9bf972c3a376f902f4d54d4e23fbde9b6
David Gomes <davidbgomes92@gmail.com>
2020-12-26 16:46:10 +0000

    Updates

--- precache-manifest.3dfa9dbf09b1834f081f5172389c2df8.js
+++ precache-manifest.3dfa9dbf09b1834f081f5172389c2df8.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7ca3fec687e6755b789bdee9e6b270bc",
+    "revision": "429135d4a88a47eb9b9b3ba247ceb2d0",
     "url": "/index.html"
   },
   {
-    "revision": "481b234168a81e58e11a",
+    "revision": "e042a99783e5d325f4ba",
     "url": "/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "53eda1a9559f59bede11",
+    "revision": "fe8b10b78324be98c722",
     "url": "/static/css/main.9db939a7.chunk.css"
   },
   {
-    "revision": "481b234168a81e58e11a",
-    "url": "/static/js/2.15911a69.chunk.js"
+    "revision": "e042a99783e5d325f4ba",
+    "url": "/static/js/2.82119e97.chunk.js"
   },
   {
-    "revision": "53eda1a9559f59bede11",
-    "url": "/static/js/main.b010ea21.chunk.js"
+    "revision": "fe8b10b78324be98c722",
+    "url": "/static/js/main.dc8b5fd6.chunk.js"
   },
   {
     "revision": "05129d9571d833ef1980",
